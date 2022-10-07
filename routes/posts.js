c918ef21bcbcd0c6b264b3f870c6391de157cb3a
Dewei <tan.dewei@hotmail.com>
2022-10-07 10:44:37 +0800

    deploy

--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.get("/", getPosts);
 router.post("/", createPost);
-router.patch("/:id", updatePost);
+// router.patch("/:id", updatePost);
 
 module.exports = router;
